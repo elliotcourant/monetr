f54088159cc4e1e3f594b668452022ec33d37f32
Elliot Courant <me@elliotcourant.dev>
2026-04-02 20:37:26 -0500

    chore: More tweaks

--- docs/components/SignIn.tsx
+++ docs/components/SignIn.tsx
@@ -6,7 +6,7 @@ export default function SignIn(): JSX.Element {
       className='btn-sm hidden sm:block text-slate-300 hover:text-white transition duration-150 ease-in-out group relative no-underline'
       href='https://my.monetr.app/'
     >
-      <span className='relative inline-flex items-center'>
+      <span className='relative inline-flex items-center text-nowrap'>
         Sign In
         <ArrowRight className='h-5 w-5' />
       </span>
