8e2a37ff72dcf203fa9ade036e30cee24081b74d
Elliot Courant <me@elliotcourant.dev>
2026-04-04 18:18:06 -0500

    chore: Adding license to borrowed code

--- docs/components/Blog/BlogIndex.tsx
+++ docs/components/Blog/BlogIndex.tsx
@@ -1,4 +1,5 @@
 /*
+  @license
   This code is adapted from https://github.com/langfuse/langfuse-docs
 
   MIT License
@@ -22,6 +23,7 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
+  @license-end
 */
 
 import { format, parse } from 'date-fns';
